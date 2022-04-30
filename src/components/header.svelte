8b391df9b025a7d4d43d2a2b8395c8a5c83250bf
gimhanul <gimhanuly@gmail.com>
2022-04-30 20:54:46 +0900

    ADD :: main page
    - header component 추가
    - main button component 추가

---
+++ src/components/header.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let title;
+    export let smallTitle;
+</script>
+
+<header>
+    <img src="images/logo.png" alt="logo">
+    <span class="yellow header--title">{ title }</span>
+    <span class="header--small-title">{ smallTitle }</span>
+</header>
+
+<style>
+    header {
+        text-align: center;
+    }
+    .header--title {
+        font-weight: 600;
+        font-size: 50px;
+        line-height: 59px;
+    }
+    .header--small-title {
+        font-weight: 400;
+        font-size: 40px;
+        line-height: 47px;
+    }
+</style>
---
+++ src/page/main.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Header from '../components/header.svelte'
+    import MainButton from '../components/mainButton.svelte'
+</script>
+
+<Header
+        title="상품권"
+        smallTitle="관리"
+/>
+<section class="main--button-list">
+    <MainButton
+            title = "발권"
+            description="매점 상품권을 발권할 수 있습니다."
+    />
+    <MainButton
+            title = "사용"
+            description="매점 상품권을 사용할 수 있습니다."
+    />
+</section>
+
+<style>
+    .main--button-list {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: nowrap;
+        gap: 83px;
+        margin-top: 4%;
+    }
+</style>
