7ecf98c0e9ee94ab9857c308c198f9ecad06506e
gimhanul <gimhanuly@gmail.com>
2022-05-03 20:26:02 +0900

    ADD :: check page
    - long button component로 분리
    - check page

---
+++ src/components/longButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let title;
+    export let color;
+    export let backgroundColor;
+</script>
+
+<button class="long-button" style="color: { color }; background: { backgroundColor }">{ title }</button>
+
+<style>
+    .long-button {
+        margin-top: 1%;
+        width: 83%;
+        height: 9%;
+        font-weight: 500;
+        font-size: 1.5em;
+        border: none;
+        box-shadow: 0px 4px 15px -2px rgba(0, 0, 0, 0.25);
+        border-radius: 40px;
+    }
+    .long-button:focus {
+        border: none;
+    }
+    .long-button:active {
+        background-color: transparent;
+        box-shadow: 0px 4px 15px -2px rgba(0, 0, 0, 0.5);
+        position: relative;
+        top:2px;
+    }
+</style>
