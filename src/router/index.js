591e81a1dcb3832145a02731bdc71f855abd8714
gimhanul <gimhanuly@gmail.com>
2022-05-04 22:37:13 +0900

    REFACTOR :: use page
    - use page 합침
    - 실패할까봐 branch 따로 파서 작업함
    - state 사용해서 page별로 data 전달 따로 안 해도 되게 만듦

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Main from '../page/main.svelte'
 import Issue from '../page/issue.svelte'
 import Use from '../page/use.svelte'
-import UseResult from '../page/useResult.svelte'
-import Check from '../page/check.svelte'
-import StudentNumber from '../page/studentNumber.svelte'
 
 const routes = {
     '/': Main,
     '/issue': Issue,
-    '/use': Use,
-    '/use/check/studentNUmber': StudentNumber,
-    '/use/check': Check,
-    '/use/check/use': UseResult
+    '/use': Use
 }
 
 export default routes;
